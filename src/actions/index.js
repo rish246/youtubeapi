a56cad038c7da86032ba7c3914181527a665dc95
rish246 <rishabhkatna2228@gmail.com>
2020-03-17 13:00:00 +0530

    fixed a bug in videoList action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,36 +19,46 @@ export const signOut = () => {
 
 // create an action creator that can make requests to youtube api and take the param to find for the val
 export const fetchVideos = (searchParam) => async (dispatch) => {
-	const response = await youtube.get('/search', {
-		params: {
-			part: 'snippet',
-			q: searchParam,
-			key: API_KEY
-		}
-	});
+	try {
+		const response = await youtube.get('/search', {
+			params: {
+				part: 'snippet',
+				q: searchParam,
+				key: API_KEY
+			}
+		});
 
-	dispatch({
-		type: FETCH_VIDEOS,
-		payload: response.data.items
-	});
+		console.log(response.data);
+
+		dispatch({
+			type: FETCH_VIDEOS,
+			payload: response.data.items
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 // getState => checkSignIn process
 // getState ........... which is not doing this [ this is just a match ]
 
 export const fetchVideo = (videoId) => async (dispatch) => {
-	const response = await youtube.get('/videos', {
-		params: {
-			part: 'snippet,contentDetails,statistics',
-			key: API_KEY,
-			id: videoId
-		}
-	});
+	try {
+		const response = await youtube.get('/videos', {
+			params: {
+				part: 'snippet,contentDetails,statistics',
+				key: API_KEY,
+				id: videoId
+			}
+		});
 
-	const video = response.data.items;
-	// const video = videos.find((video) => video.id.videoId === videoId);
-	dispatch({
-		type: FETCH_VIDEO,
-		payload: video
-	});
+		const video = response.data.items;
+		// const video = videos.find((video) => video.id.videoId === videoId);
+		dispatch({
+			type: FETCH_VIDEO,
+			payload: video
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
